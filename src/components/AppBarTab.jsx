85b619d1c2deaad487a3c8afbc5edf161cdc3943
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-12-24 13:06:47 +0200

    10.7 done

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -4,7 +4,7 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   View,
-  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import Text from './Text';
 import { Link } from 'react-router-native';
@@ -28,20 +28,22 @@ const AppBar = () => {
   return (
     <>
       <View style={styles.container}>
-        <Link to="/" activeOpacity={0.8} component={TouchableWithoutFeedback}>
-          <Text color="textWhite" fontSize="heading" fontWeight="bold">
-            Repositories
-          </Text>
-        </Link>
-        <Link
-          to="/signin"
-          activeOpacity={0.8}
-          component={TouchableWithoutFeedback}
-        >
-          <Text color="textWhite" fontSize="heading" fontWeight="bold">
-            Sign In
-          </Text>
-        </Link>
+        <ScrollView horizontal>
+          <Link to="/" activeOpacity={0.8} component={TouchableWithoutFeedback}>
+            <Text color="textWhite" fontSize="heading" fontWeight="bold">
+              Repositories
+            </Text>
+          </Link>
+          <Link
+            to="/signin"
+            activeOpacity={0.8}
+            component={TouchableWithoutFeedback}
+          >
+            <Text color="textWhite" fontSize="heading" fontWeight="bold">
+              Sign In
+            </Text>
+          </Link>
+        </ScrollView>
       </View>
     </>
   );
