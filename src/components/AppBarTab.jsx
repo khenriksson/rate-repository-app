9ba9d07951816b43be71022e071cd2ee64d0ddf2
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-12-24 13:01:28 +0200

    10.6 done

--- src/components/AppBarTab.jsx
+++ src/components/AppBarTab.jsx
@@ -1,29 +1,49 @@
 import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+  TouchableOpacity,
+} from 'react-native';
 import Text from './Text';
+import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     height: 150,
     backgroundColor: '#24292e',
-    justifyContent: 'flex-end',
+
     paddingBottom: Constants.statusBarHeight,
     paddingLeft: Constants.statusBarHeight / 2,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
   // ...
 });
 
 const AppBar = () => {
   return (
-    <TouchableWithoutFeedback>
+    <>
       <View style={styles.container}>
-        <Text color="textWhite" fontSize="heading" fontWeight="bold">
-          Repositories
-        </Text>
+        <Link to="/" activeOpacity={0.8} component={TouchableWithoutFeedback}>
+          <Text color="textWhite" fontSize="heading" fontWeight="bold">
+            Repositories
+          </Text>
+        </Link>
+        <Link
+          to="/signin"
+          activeOpacity={0.8}
+          component={TouchableWithoutFeedback}
+        >
+          <Text color="textWhite" fontSize="heading" fontWeight="bold">
+            Sign In
+          </Text>
+        </Link>
       </View>
-    </TouchableWithoutFeedback>
+    </>
   );
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,8 @@ import { StyleSheet, View } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
+import { Route, Switch, Redirect } from 'react-router-native';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -17,7 +19,15 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBarTab />
-      <RepositoryList />
+      <Switch>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Route path="/signin" exact>
+          <SignIn />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
